7cd374d95e7c7588c94948f4506c9d495f5e977c
RONEL <148021522+LilRonz@users.noreply.github.com>
2025-05-19 22:43:59 +0800

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://usermanagementfinal-production.up.railway.app'
+    apiUrl: 'https://user-management-introg-production.up.railway.app'
 }
